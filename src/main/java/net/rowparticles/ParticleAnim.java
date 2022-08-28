72cd1b4c5d4236c132e44d7a47f4bec6fa4b60ea
rownox <rownox.ik@gmail.com>
2022-08-27 20:33:38 -0400

    time

--- src/main/java/net/rowparticles/ParticleAnim.java
+++ src/main/java/net/rowparticles/ParticleAnim.java
@@ -9,21 +9,21 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class ParticleAnim {
     public static void drawParticle(Player p, Plugin plugin, int R, int G, int B) {
         new BukkitRunnable() {
-            double t = 0;
+            double time = 0;
             Location loc = p.getLocation();
             public void run() {
-                t = t + 0.1 * Math.PI;
+                time = time + 0.1 * Math.PI;
 
                 for (double theta = 0; theta <= 2 * Math.PI; theta = theta + Math.PI/35) {
-                    double x = t * Math.cos(theta);
-                    double y = Math.exp(0.1 * t) * Math.sin(t) * 1.5;
-                    double z = t * Math.sin(theta);
+                    double x = time * Math.cos(theta);
+                    double y = Math.exp(0.1 * time) * Math.sin(time) * 1.5;
+                    double z = time * Math.sin(theta);
 
                     loc.add(x, y, z);
                     loc.getWorld().spawnParticle(Particle.REDSTONE, loc, 1, new Particle.DustOptions(org.bukkit.Color.fromRGB(R, G, B), 2));
                     loc.subtract(x, y, z);
                 }
-                if (t > 3) {
+                if (time > 3) {
                     this.cancel();
                 }
             }
